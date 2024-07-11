78b6f71f73b4a7760d61a6eeef89f27a15d33704
Ifeanyi Nwocha <ifeanyinwocha@gmail.com>
2024-07-11 18:58:53 +0200

    dancer dashboard done

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
     resources :dancer_auditions, only: [:create, :destroy]
   end
 
+  get 'my_auditions', to: 'dancers#my_auditions', as: 'my_auditions'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
