bd1b66cd02c5c1253d4a3515a995998c29e64a1d
Lood Rabie <lood0518@gmail.com>
2024-07-17 14:59:39 +0200

    fixed profile picture on dancer show page, added dancing styles

--- app/controllers/dancers_controller.rb
+++ app/controllers/dancers_controller.rb
@@ -11,10 +11,10 @@ class DancersController < ApplicationController
   end
 
   def edit
+    @dancer = Dancer.find(params[:id])
   end
 
   def show
-
   end
 
   def create
@@ -29,7 +29,7 @@ class DancersController < ApplicationController
   def update
     @dancer = Dancer.find(params[:id])
     if @dancer.update(dancer_params)
-      redirect_to dancer_path(@dancer), notice: 'Dancer profile was successfully updated.'
+      redirect_to @dancer, notice: 'Dancer profile was successfully updated.'
     else
       render :edit
     end
@@ -52,6 +52,6 @@ class DancersController < ApplicationController
   end
 
   def dancer_params
-    params.require(:dancer).permit(:name, :age, :location, :bio)
+    params.require(:dancer).permit(:name, :age, :location, :bio, :dancing_styles, :profile_picture)
   end
 end
